f7f88695b20b473bd7fb66e61188f3a90d39d9d7
vvnha <vvnha.17it3@sict.udn.vn>
2022-09-06 16:00:51 +0700

    update proxy

---
+++ pages/api/login.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import httpProxy from 'http-proxy';
+
+// turrn off bodyParser => bodyParser are handled in server
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const proxy = httpProxy.createProxyServer();
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  return new Promise((resolve) => {
+    // dont send cookies to API server
+    req.headers.cookie = '';
+
+    //remove /api to avoid /api/v1/api/products?_page=1&_limit=4
+    if (req.url?.startsWith('/api')) {
+      req.url = req.url.replace('/api', '');
+    }
+
+    proxy.web(req, res, {
+      target: process.env.API_URL,
+      changeOrigin: true,
+      selfHandleResponse: false,
+    });
+
+    proxy.once('proxyRes', () => {
+      resolve(true);
+    });
+  });
+}
