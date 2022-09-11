dbd2b05df08c44273ba3ea45fbc959c9aa6756f0
vvnha <vvnha.17it3@sict.udn.vn>
2022-09-11 16:19:59 +0700

    features/development -update- cart page

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,6 +1,6 @@
 import CartList from '@/components/cart/cart-list';
 import { MainLayout } from '@/components/layouts';
-import { Box, Container, Stack, Typography } from '@mui/material';
+import { BottomNavigation, Box, Button, Container, Stack, Typography } from '@mui/material';
 import * as React from 'react';
 
 export interface CartPageProps {}
@@ -9,7 +9,12 @@ export default function CartPage(props: CartPageProps) {
   return (
     <Box py={2}>
       <Container>
-        <Stack direction="column">
+        <Stack
+          direction="column"
+          sx={{
+            position: 'relative',
+          }}
+        >
           <Typography
             sx={{
               fontFamily: 'Pangolin',
@@ -22,6 +27,37 @@ export default function CartPage(props: CartPageProps) {
             Your cart
           </Typography>
           <CartList />
+          <Box sx={{ position: 'sticky', bottom: 0, left: 0, right: 0 }}>
+            <BottomNavigation
+              showLabels
+              sx={{
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}
+            >
+              <Stack direction="column">
+                <Typography>Total:</Typography>
+                <Typography
+                  sx={{
+                    fontFamily: 'Rubik',
+                    fontWeight: '400',
+                    fontStyle: 'normal',
+                    fontSize: '18px',
+                    lineHeight: '18px',
+                    color: 'primary.main',
+                    display: 'inline',
+                  }}
+                >
+                  $123.00
+                </Typography>
+              </Stack>
+              <Stack>
+                <Button variant="contained" size="large">
+                  Buy
+                </Button>
+              </Stack>
+            </BottomNavigation>
+          </Box>
         </Stack>
       </Container>
     </Box>
